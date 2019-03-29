ce7cceb821198de4619e17b4f5a7d99e27330a74
splaa <splaandrey@gmail.com>
2019-03-29 14:07:57 +0100

    Initial start app

---
+++ bootstrap/app.php
@@ -0,0 +1,5 @@
+<?php
+	define('BASE_PATH', dirname(__DIR__));
+	
+	dd(BASE_PATH);
+	var_dump('Hello');
--- public/index.php
+++ public/index.php
@@ -5,3 +5,9 @@
 	 * Date: 28.03.19
 	 * Time: 10:07
 	 */
+	
+	require_once __DIR__ . '/../vendor/autoload.php';
+	
+	
+	$app = require __DIR__ . '/../bootstrap/app.php';
+	$app->run();
